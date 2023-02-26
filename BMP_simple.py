8851b975202e5bc891692ce503b08d2f388ad7fe
TecnoLògics Bellvitge <tecnologics@ibellvitge.net>
2023-02-26 07:39:37 +0100

    Update BMP_simple.py

--- BMP_simple.py
+++ BMP_simple.py
@@ -1,16 +1,16 @@
 # importa biblioteques:
 import time
-import lib.BMP280 as bmp280  # arxiu BMP280 que hem creat dins la carpeta lib
+import libs.BMP280 as bmp280  # arxiu BMP280 que hem creat dins la carpeta lib
 
 # configuracio:
 sensor = bmp280.BMP280()  # definicio de l'objecte BMP280 amb el nom sensor
 
-try:    # execucio en condicions normals
+try:
   while True:
-	temperatura = sensor.get_temperature()  # retorna la temperatura en C
-      	pressio = sensor.get_pressure()     # retorna la pressio en hPa
-	print ("temperatura: %s C, pressio: %s hPa" % (temperatura, pressio))  # imprimeix 
-	time.sleep(1)	# interval actualitzacio de dades 1s
+    temperatura=sensor.get_temperature()
+    pressio=sensor.get_pressure()
+    print("temperatura: %s C, pressio: %s hPa" % (temperatura, pressio))
+    time.sleep(1)
 
-except:    # quan es produeixi una excepcio, com Ctrl+C, l’execucio sortira del bucle anterior
-  print ("execucio interrompuda")
+except:
+  print("execucio interrompuda")
