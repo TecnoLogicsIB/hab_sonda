107347d5d5dbc39f5b94adf4e9dbfd95866da5c4
TecnoLògics Bellvitge <tecnologics@ibellvitge.net>
2023-02-26 07:09:52 +0100

    Update BMP_simple.py

--- BMP_simple.py
+++ BMP_simple.py
@@ -5,7 +5,7 @@ import lib.BMP280 as bmp280  # arxiu BMP280 que hem creat dins la carpeta lib
 # configuracio:
 sensor = bmp280.BMP280()  # definicio de l'objecte BMP280 amb el nom sensor
 
-try:    # execució en condicions normals
+try:    # execucio en condicions normals
   while True:
 	temperatura = sensor.get_temperature()  # retorna la temperatura en C
       	pressio = sensor.get_pressure()     # retorna la pressio en hPa
